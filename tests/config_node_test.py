1df4684f2eab756f6e134eaf68247f1f6cffb636
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-12-05 15:12:44 +0100

    Refs #7: add test for contains check

--- tests/config_node_test.py
+++ tests/config_node_test.py
@@ -69,3 +69,11 @@ class ConfigNodeTest(TestCase):
         self.assertEqual(pickle_conf.key1, 1)
         self.assertEqual(pickle_conf.key2.key3, 3)
         self.assertEqual(pickle_conf.key2.key4.key5, 5)
+
+    def test_contains(self):
+        config = ConfigNode(self.config_dict)
+        self.assertTrue('key1' in config)
+        self.assertFalse('key1.key2' in config)
+        self.assertTrue('key2.key3' in config)
+        self.assertTrue('key2.key4.key5' in config)
+        self.assertFalse('key2.key4.key6' in config)
