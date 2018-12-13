2e92cb88583a724ffae8f8e07335e6f9650a7c41
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-12-13 13:29:11 +0100

    Closes #8: Handling of optional values (#17) (#18)
    * Refs #8: add optional and required fields to config node
    
    * Refs #8: provide builder methods for optional and required access
    
    * Refs #8: update readme
    
    * Refs #8: Version to 1.1.1
    
    * Fix badge link in readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.com/janehmueller/python-config.svg?token=tGKCTy4zTZfGNfjpEgEX&branch=master)](https://travis-ci.com/janehmueller/python-config)
+[![Build Status](https://travis-ci.com/janehmueller/python-json-config.svg?token=tGKCTy4zTZfGNfjpEgEX&branch=master)](https://travis-ci.com/janehmueller/python-json-config)
 ![License](https://img.shields.io/pypi/l/python-json-config.svg)
 [![Version](https://img.shields.io/pypi/v/python-json-config.svg)](https://pypi.python.org/pypi/python-json-config/)
 
@@ -13,37 +13,76 @@ pip install python-json-config
 # Usage
 ```
 from python_json_config import ConfigBuilder
-from python_json_config.validators import is_unreserved_port, is_ipv4_address, is_timedelta
-from python_json_config.transformers import to_timedelta
-from datetime import datetime, timedelta
 
 # create config parser
 builder = ConfigBuilder()
 
-# assert that port contains an int value
+# parse config
+config = builder.parse_config('path/to/config.json')
+
+# access elements
+host = config.server.host
+port = config.server.port
+myfield = config.myfield
+```
+
+## Validate field types
+```
 builder.validate_field_type('server.ip', str)
 builder.validate_field_type('server.port', int)
 builder.validate_field_type('jwt.access_token_expires', str)
+```
 
-# assert that the port is not a reserved port 
+## Validate field values
+```
+from python_json_config.validators import is_unreserved_port, is_ipv4_address, is_timedelta
+
+# use provided methods
 builder.validate_field_value('server.ip', is_ipv4_address)
 builder.validate_field_value('server.port', is_unreserved_port)
 builder.validate_field_value('jwt.access_token_expires', is_timedelta)
 
-# return custom error messages in your lambdas
+# use custom validation function
+builder.validate_field_value('server.ip', lambda ip: ip != '0.0.0.0')
+
+# return custom error messages in your lambda
 builder.validate_field_value('server.ip', lambda ip: (ip != '0.0.0.0', 'IP is unroutable.'))
 
 # chain validation functions
 builder.validate_field_value('server.ip', [lambda ip: ip != 'localhost', lambda ip: ip != '127.0.0.1'])
+```
+
+## Transform field values
+```
+from python_json_config.transformers import to_timedelta
+
+# use provided methods
+builder.transform_field_value('jwt.access_token_expires', to_timedelta)
+
+
+from datetime import datetime
 
 # parse a timedelta (e.g., Jun 1 2005) into a datetime object
 builder.transform_field_value('important_date', lambda date: datetime.strptime(date, '%b %d %Y'))
-builder.transform_field_value('jwt.access_token_expires', to_timedelta)
+```
 
-# parse config
-config = builder.parse_config('path/to/config.json')
+## Define field access settings
+```
+# required means an error is thrown if a non-existing field is accessed 
+builder.set_field_access_required()
+# return None for the following fields instead of throwing an error
+builder.add_optional_field('server.host')
+builder.add_optional_fields(['cache.ttl', 'server.path'])
 
-# access config values
+# optional means None is returned if a non-existing field is accessed 
+builder.set_field_access_optional()
+# throw an error for the following fields instead of returning None
+builder.add_required_field('server.user')
+builder.add_required_fields(['cache.backend', 'server.password'])
+```
+
+## Access config values
+```
 port = config.server.port
 assert port > 1023
 
--- python_json_config/config_builder.py
+++ python_json_config/config_builder.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, Union
+from typing import Dict, Union, List
 
 import jsonschema
 
@@ -8,15 +8,28 @@ from .config_node import Config
 
 class ConfigBuilder(object):
     def __init__(self):
+        self.__config: Config = None
+
+        # custom validation and transformation settings
         self.__validation_types: Dict[str, type] = {}
         self.__validation_functions: Dict[str, list] = {}
         self.__transformation_functions = {}
-        self.__config: Config = None
+
+        # stores the json schema used to validate the config
         self.__json_schema: dict = None
+
+        # settings of required and optional fields and (non-)strict access
+        self.__strict_access: bool = None
+        self.__field_access_settings: Dict[str, bool] = {}
 
     def validate_field_type(self, field_name: str, field_type: type):
         self.__validation_types[field_name] = field_type
         return self
+
+    def __validate_types(self):
+        for field_name, field_type in self.__validation_types.items():
+            value = self.__config.get(field_name)
+            assert isinstance(value, field_type), f'Config field "{field_name}" with value "{value}" is not of type {field_type}'
 
     def validate_field_value(self, field_name: str, validation_function):
         if field_name not in self.__validation_functions:
@@ -28,38 +41,6 @@ class ConfigBuilder(object):
             self.__validation_functions[field_name].append(validation_function)
 
         return self
-
-    def transform_field_value(self, field_name: str, transformation_function):
-        self.__transformation_functions[field_name] = transformation_function
-        return self
-
-    def validate_with_schema(self, schema: Union[str, dict]):
-        if isinstance(schema, dict):
-            self.__json_schema = schema
-        else:
-            with open(schema, "r") as json_file:
-                self.__json_schema = json.load(json_file)
-
-    def parse_config(self, config: Union[str, dict]) -> Config:
-        if isinstance(config, dict):
-            config_dict = config
-        else:
-            with open(config, "r") as json_file:
-                config_dict = json.load(json_file)
-
-        if self.__json_schema is not None:
-            jsonschema.validate(config_dict, self.__json_schema)
-        self.__config = Config(config_dict)
-        self.__validate_types()
-        self.__validate_field_values()
-        self.__transform_field_values()
-
-        return self.__config
-
-    def __validate_types(self):
-        for field_name, field_type in self.__validation_types.items():
-            value = self.__config.get(field_name)
-            assert isinstance(value, field_type), f'Config field "{field_name}" with value "{value}" is not of type {field_type}'
 
     def __validate_field_values(self):
         for field_name, validation_functions in self.__validation_functions.items():
@@ -74,8 +55,67 @@ class ConfigBuilder(object):
                 else:
                     assert validation_result, error_message
 
+    def transform_field_value(self, field_name: str, transformation_function):
+        self.__transformation_functions[field_name] = transformation_function
+        return self
+
     def __transform_field_values(self):
         for field_name, transformation_function in self.__transformation_functions.items():
             value = self.__config.get(field_name)
             new_value = transformation_function(value)
             self.__config.update(field_name, new_value)
+
+    def validate_with_schema(self, schema: Union[str, dict]):
+        """
+        Save the jsonschema for later validation.
+        :param schema: The jsonschema with which the config will be validated. Can be either a file path to a JSON file
+                       containing the schema or an already parsed dictionary.
+        """
+        if isinstance(schema, dict):
+            self.__json_schema = schema
+        else:
+            with open(schema, "r") as json_file:
+                self.__json_schema = json.load(json_file)
+
+    def set_field_access_optional(self):
+        self.__strict_access = False
+
+    def set_field_access_required(self):
+        self.__strict_access = True
+
+    def add_required_field(self, field_name: str):
+        self.__field_access_settings[field_name] = True
+
+    def add_required_fields(self, field_names: List[str]):
+        for field in field_names:
+            self.add_required_field(field)
+
+    def add_optional_field(self, field_name: str):
+        self.__field_access_settings[field_name] = False
+
+    def add_optional_fields(self, field_names: List[str]):
+        for field in field_names:
+            self.add_optional_field(field)
+
+    def parse_config(self, config: Union[str, dict]) -> Config:
+        if isinstance(config, dict):
+            config_dict = config
+        else:
+            with open(config, "r") as json_file:
+                config_dict = json.load(json_file)
+
+        if self.__json_schema is not None:
+            jsonschema.validate(config_dict, self.__json_schema)
+        self.__config = Config(config_dict,
+                               strict_access=self.__strict_access,
+                               required_fields=[field for field, status in self.__field_access_settings.items() if status],
+                               optional_fields=[field for field, status in self.__field_access_settings.items() if not status])
+        self.__validate_types()
+        self.__validate_field_values()
+        self.__transform_field_values()
+
+        return self.__config
+
+
+
+
