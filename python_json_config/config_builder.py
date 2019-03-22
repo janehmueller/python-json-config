6824d1cac71491a121a5e5b746430e8e65e84dba
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-22 19:10:25 +0100

    Add documentation to config builder

--- python_json_config/config_builder.py
+++ python_json_config/config_builder.py
@@ -23,6 +23,12 @@ class ConfigBuilder(object):
         self.__field_access_settings: Dict[str, bool] = {}
 
     def validate_field_type(self, field_name: str, field_type: type):
+        """
+        Validate that the given field is of the given type when the final config is built.
+        :param field_name: The field that is validated.
+        :param field_type: The type that the field value should have.
+        :return: The builder object for chaining of calls.
+        """
         self.__validation_types[field_name] = field_type
         return self
 
@@ -32,9 +38,18 @@ class ConfigBuilder(object):
             # skip optional fields that do not exist
             if value is None:
                 continue
-            assert isinstance(value, field_type), f'Config field "{field_name}" with value "{value}" is not of type {field_type}'
+            assert isinstance(value, field_type), f'Config field "{field_name}" with value "{value}" is not of ' \
+                f'type {field_type}'
 
     def validate_field_value(self, field_name: str, validation_function):
+        """
+        Validate that the validation function returns true with the value of the given field when the final config is
+        built.
+        :param field_name: The field that is validated.
+        :param validation_function: Function that takes the field value as input and validates it (returns True if the
+                                    value is valid and False if it is invalid).
+        :return: The builder object for chaining of calls.
+        """
         if field_name not in self.__validation_functions:
             self.__validation_functions[field_name] = []
 
@@ -62,6 +77,14 @@ class ConfigBuilder(object):
                     assert validation_result, error_message
 
     def transform_field_value(self, field_name: str, transformation_function):
+        """
+        Transform the given field value with the transformation function when the final config is built.
+        The transformation function takes the field value as input and returns a new value.
+        :param field_name: The field that is validated.
+        :param transformation_function: Function that takes the field value as input and transforms it into another
+                                        value.
+        :return: The builder object for chaining of calls.
+        """
         self.__transformation_functions[field_name] = transformation_function
         return self
 
@@ -87,26 +110,54 @@ class ConfigBuilder(object):
                 self.__json_schema = json.load(json_file)
 
     def set_field_access_optional(self):
+        """
+        Set the access mode of all fields to optional (if the field doesn't exist, None is returned).
+        """
         self.__strict_access = False
 
     def set_field_access_required(self):
+        """
+        Set the access mode of all fields to required (if the field doesn't exist, an exception is raised).
+        """
         self.__strict_access = True
 
     def add_required_field(self, field_name: str):
+        """
+        Set the access mode of the given field to required (if the field doesn't exist, an exception is raised).
+        :param field_name: The field whose access mode is set to required.
+        """
         self.__field_access_settings[field_name] = True
 
     def add_required_fields(self, field_names: List[str]):
+        """
+        Set the access mode of the given fields to required (if the field doesn't exist, an exception is raised).
+        :param field_names: List of fields whose access mode is set to required.
+        """
         for field in field_names:
             self.add_required_field(field)
 
     def add_optional_field(self, field_name: str):
+        """
+        Set the access mode of the given field to optional (if the field doesn't exist, None is returned).
+        :param field_name: The field whose access mode is set to required.
+        """
         self.__field_access_settings[field_name] = False
 
     def add_optional_fields(self, field_names: List[str]):
+        """
+        Set the access mode of the given fields to optional (if the field doesn't exist, None is returned).
+        :param field_names: List of fields whose access mode is set to required.
+        """
         for field in field_names:
             self.add_optional_field(field)
 
     def parse_config(self, config: Union[str, dict]) -> Config:
+        """
+        Build the config. This method should be called last and uses the settings set via the other methods of this
+        class (e.g., validate field type).
+        :param config: Path to the config json file or a dictionary that contains the config values
+        :return: The built config (that is validated and transformed according to the passed functions).
+        """
         if isinstance(config, dict):
             config_dict = config
         else:
@@ -117,8 +168,10 @@ class ConfigBuilder(object):
             jsonschema.validate(config_dict, self.__json_schema)
         self.__config = Config(config_dict,
                                strict_access=self.__strict_access,
-                               required_fields=[field for field, status in self.__field_access_settings.items() if status],
-                               optional_fields=[field for field, status in self.__field_access_settings.items() if not status])
+                               required_fields=[field for field, status in self.__field_access_settings.items()
+                                                if status],
+                               optional_fields=[field for field, status in self.__field_access_settings.items()
+                                                if not status])
         self.__validate_types()
         self.__validate_field_values()
         self.__transform_field_values()
